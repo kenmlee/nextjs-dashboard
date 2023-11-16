b3ac81aaf5167e6f8d896af9ca9e936f84a37028
Ken Lee <limingzhe.cn@gmail.com>
2023-11-16 09:39:32 +0800

    finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'description in metadata',
+  metadataBase: new URL('http://mini.lan:3000')
+}
 
 export default function RootLayout({
   children,
